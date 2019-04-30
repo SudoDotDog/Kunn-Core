3bf2db1352cb9bd734b3acca219d48f91ecc0366
WMXPY <wm@mengw.io>
2019-04-29 19:08:13 -0500

    finish up validation function

--- test/unit/config/validate.test.ts
+++ test/unit/config/validate.test.ts
@@ -26,4 +26,59 @@ describe('Given [ConfigValidation] helper methods', (): void => {
         // tslint:disable-next-line
         expect(result).to.be.true;
     });
+
+    it('should be able to validate array kunn data', (): void => {
+
+        const pattern: KunnData = {
+            type: TYPE.ARRAY,
+            element: {
+                type: TYPE.INTEGER,
+            },
+        };
+
+        const result: boolean = validateKunnData(pattern);
+
+        // tslint:disable-next-line
+        expect(result).to.be.true;
+    });
+
+    it('should be able to validate object kunn data', (): void => {
+
+        const pattern: KunnData = {
+            type: TYPE.OBJECT,
+            map: {
+                [chance.string()]: {
+                    type: TYPE.ARRAY,
+                    element: {
+                        type: TYPE.FLOAT,
+                    },
+                },
+            },
+        };
+
+        const result: boolean = validateKunnData(pattern);
+
+        // tslint:disable-next-line
+        expect(result).to.be.true;
+    });
+
+    it('should be able to fail object kunn data', (): void => {
+
+        const pattern: KunnData = {
+            type: TYPE.OBJECT,
+            map: {
+                [chance.string()]: {
+                    type: TYPE.ARRAY,
+                    element: {
+                        type: chance.string(),
+                    } as any,
+                },
+            },
+        };
+
+        const result: boolean = validateKunnData(pattern);
+
+        // tslint:disable-next-line
+        expect(result).to.be.false;
+    });
 });

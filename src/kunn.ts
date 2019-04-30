7863a8b77e9cf2af57f6c6bbc65f3e57ab8e4106
WMXPY <wm@mengw.io>
2019-04-30 16:50:42 -0500

    0.0.3 add valdation and route function

--- src/kunn.ts
+++ src/kunn.ts
@@ -4,15 +4,26 @@
  * @description Kunn
  */
 
+import { validateKunnConfig } from "./config/validate";
 import { PROTOCOL } from "./declare/declare";
 import { KunnConfig } from "./declare/kunn";
+import { KunnRoute } from "./declare/route";
 import { Agent } from "./routes/agent";
 
 export class Kunn {
 
-    public static fromConfig(config: KunnConfig): Kunn {
+    public static fromAny(config: KunnConfig): Kunn {
 
         return new Kunn(config);
+    }
+
+    public static fromConfig(config: KunnConfig): Kunn | null {
+
+        const kunn: Kunn = new Kunn(config);
+        if (kunn.validate()) {
+            return kunn;
+        }
+        return null;
     }
 
     private readonly _config: KunnConfig;
@@ -25,6 +36,16 @@ export class Kunn {
     public get config(): KunnConfig {
 
         return this._config;
+    }
+
+    public validate(): boolean {
+
+        return validateKunnConfig(this._config);
+    }
+
+    public routes(): KunnRoute[] {
+
+        return this._config.routes;
     }
 
     public match<P extends PROTOCOL>(path: string, protocol: P): Agent<P> | null {
